b259284fd1be192d811c938e057bc24c841ecf75
Viet Nguyen <111045516+vietnguyendl61@users.noreply.github.com>
2024-01-10 15:04:31 +0700

    day one
    - env
    - view engine

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-asdcasd
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+const port = process.env.PORT || 8001
+const host_name = process.env.HOST_NAME || 'localhost'
+
+app.set('views', './src/views/')
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.render('sample.ejs')
+})
+
+
+app.listen(port, host_name, () => {
+    console.log(`Example app listening on port ${port}`)
+})
